41511480a9ea0d377e1347759ad958d972ee05d9
frankgui <frank_gui@icloud.com>
2019-06-27 19:56:05 +0800

    first commit

---
+++ src/TestDemo.java
@@ -0,0 +1,9 @@
+/**
+ * @author frankgui 2019-06-27 19:53
+ * @version V1.0
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
